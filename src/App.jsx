16ec34973fd47cbc9685da06443d1655c39edb24
michaelogunn <mogunnaike96@gmail.com>
2025-11-26 17:37:37 +0000

    Mongo lab Integrated Tasky React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
 import './App.css';
 import Task from './components/Task';
-import React,{useState} from 'react';
 import AddTaskForm from './components/Form';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from 'react';
+import {getTasks, addTask, deleteTask, updateTask} from "./api/tasky-api";
 
 function App() {
-    const [ taskState, setTaskState ] = useState({
-    tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
-    ]
-  });
-
-const [ formState, setFormState ] = useState({
-    title: "",
-    description: "",
-    deadline: "",
-    priority: ""
-  });
   
+  const [ taskState, setTaskState ] = useState({tasks: []});
+
+  useEffect(() => {
+      getTasks().then(tasks => {
+        setTaskState({tasks: tasks});
+      });
+    }, []);	
+  
+    const [ formState, setFormState ] = useState({
+      title: "",
+      description: "",
+      deadline: "",
+      priority: "Low"
+    });
+  
+  //console.log(formState);
 
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
+    updateTask(tasks[taskIndex]);
     setTaskState({tasks});
-    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
-  const priorityHandler = (event) => {
-    let form = {...formState};
-    form.priority = event.target.value;
-    setFormState(form);
-  }
-    
+
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
+    const id=tasks[taskIndex]._id;
     tasks.splice(taskIndex, 1);
+    deleteTask(id);
     setTaskState({tasks});
   }
+
   const formChangeHandler = (event) => {
     let form = {...formState};
 
@@ -53,48 +53,41 @@ const [ formState, setFormState ] = useState({
           break;
       case "priority":
           form.priority = event.target.value;
-          break;  // Added this case to handle priority selection in the form submission
+          break;      
       default:
           form = formState;
     }
     setFormState(form);
   }
-    const formSubmitHandler = (event) => {
+
+  const formSubmitHandler = async (event) => {
     event.preventDefault();
-
-    const tasks = [...taskState.tasks];
+    const tasks = taskState.tasks?[...taskState.tasks]:[];
     const form = {...formState};
-
-    form.id = uuidv4();
-    
-    tasks.push(form);
+    const newTask = await addTask(form);
+    tasks.push(newTask);
     setTaskState({tasks});
   }
- 
-    console.log(formState);
-
-
-    return (
+  
+  return (
     <div className="container">
       <h1>Tasky</h1>
-  {taskState.tasks.map((task,index) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      priority={task.priority}  // Added this prop to the Task component to display priority in the task details displayx
-      key={task.id}
-      done={task.done}  // Added this prop to the Task component
-      markDone= {() => doneHandler(index)}
-      deleteTask= {() => deleteHandler(index)}  // Added this prop to the Task component
-    />
+      {taskState.tasks.map((task, index) => (              
+        <Task 
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task._id}
+          done={task.done}
+          priority={task.priority}
+          markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
+        />
+      ))}
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
 
-  ))} 
-    <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
-
-
 }
 
 export default App;
